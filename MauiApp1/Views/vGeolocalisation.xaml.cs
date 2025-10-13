4a4ce8711e9999abf0917135b21f6ac159809f9b
Elinor Tchamo <tchamoelii@gmail.com>
2025-10-13 08:57:21 +0200

    Add project files.

---
+++ MauiApp1/Views/vGeolocalisation.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.Views;
+
+public partial class vGeolocalisation : ContentPage
+{
+	public vGeolocalisation()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp1/Views/vPhone.xaml.cs
@@ -0,0 +1,26 @@
+namespace MauiApp1.Views;
+
+public partial class vPhone : ContentPage
+{
+	public vPhone()
+	{
+		InitializeComponent();
+	}
+	private void phone_Info()
+	{
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+		sb.AppendLine($"Modèle: {DeviceInfo.Current.Model}");
+        sb.AppendLine($"Modèle: {DeviceInfo.Current.Model.ToString()}");
+        sb.AppendLine($"Constructeur: {DeviceInfo.Current.Manufacturer}");
+        sb.AppendLine($"Nom: {DeviceInfo.Current.Name}");
+        sb.AppendLine($"Os {DeviceInfo.Current.VersionString}");
+        sb.AppendLine($"Platform: {DeviceInfo.Current.Platform}");
+        sb.AppendLine($"Materiel:{Get_Idiom()}");
+        lbPhoneInfo.Text=sb.ToString();
+    }
+    private string Get_Idiom()
+    {
+        return "";
+
+    }
+}
