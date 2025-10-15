0b6deedfe4c2d59c8b7045ce3dca90cf16d94e68
Elinor Tchamo <tchamoelii@gmail.com>
2025-10-15 09:03:55 +0200

    Feat: geolocation implementation

--- MauiApp1/Views/vBatterie.xaml.cs
+++ MauiApp1/Views/vBatterie.xaml.cs
@@ -27,6 +27,34 @@ public partial class vBatterie : ContentPage
                 break;
         }
         lb_Alim.Text = "Alimentation: " + stext;
+    }
 
+    private async void Battery_ModeEcoChanged()
+    {
+
+    }
+
+    private void Battery_InfoChanged(object sender, BatteryInfoChangedEventArgs e)
+    {
+        string stext = "";
+        switch (e.State)
+        {
+            case BatteryState.Full:
+                stext = "Charge maximale";
+                break;
+            case BatteryState.Charging:
+                stext = "En charge";
+                break;
+            case BatteryState.Discharging:
+                stext = "Decharge";
+                break;
+            case BatteryState.NotPresent:
+                stext = "Pas de batterie";
+                break;
+            case BatteryState.Unknown:
+                stext = "INconnu";
+                break
+        }
+        lb_Alim.Text = "Alimentation: " + stext;
     }
 }
--- MauiApp1/Views/vGeolocalisation.xaml.cs
+++ MauiApp1/Views/vGeolocalisation.xaml.cs
@@ -2,8 +2,36 @@ namespace MauiApp1.Views;
 
 public partial class vGeolocalisation : ContentPage
 {
+	private CancellationTokenSource _cancelTkenSource;
+	private bool _isCheckingLocation;
 	public vGeolocalisation()
 	{
 		InitializeComponent();
+	}
+
+	private void Geolocalisation_click(object sender, EventArgs e)
+	{
+
+	}
+
+	public async Task GetCurrentLocationn()
+	{
+		try
+		{
+			_isCheckingLocation = true;
+			GeolocationRequest geolocationRequest = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+			_cancelTkenSource = new CancellationTokenSource();
+			Location loc = await Geolocation.Default.GetLocationAsync(geolocationRequest, _cancelTkenSource.Token);
+			if (loc != null)
+			{
+				lb_longitude.Text = loc.Longitude.ToString();
+                lb_latitude.Text = loc.Latitude.ToString();
+			}
+		}
+		catch (Exception ex) { }
+		finally
+		{
+			_isCheckingLocation = false;
+		}
 	}
 }
